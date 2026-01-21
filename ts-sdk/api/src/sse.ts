cddbfe71ab5f2720859eaceada9d3bb05b104a78
codercms <coder1994@gmail.com>
2026-01-21 11:44:50 +0300

    feat: Support new OpenAPI spec

--- ts-sdk/api/src/sse.ts
+++ ts-sdk/api/src/sse.ts
@@ -2,15 +2,15 @@ import { EventSource } from "eventsource";
 
 import {
   getSseUrl,
-  NotificationDtoLendingPositionDto,
-  NotificationDtoOrderHistoryEntryDto,
-  NotificationDtoPoolSwapDto,
-  NotificationDtoStakingPositionDto,
-  NotificationDtoTradeHistoryEntryDto,
   NotificationEntity,
-  SnapshotContainer,
   SseResponse,
-  StreamInitalMessage,
+  SseResponseInitialMessage,
+  SseResponseLendingPosition,
+  SseResponseOrderHistoryEntry,
+  SseResponsePoolSwap,
+  SseResponseSnapshot,
+  SseResponseStakingPosition,
+  SseResponseTradeHistoryEntry,
 } from "./gen/defiTunaAPI";
 import { resolveTunaUrl } from "./baseUrl";
 
@@ -19,30 +19,30 @@ export const getSseUpdatesStream = async (): Promise<EventSource> => {
   return new EventSource(url);
 };
 
-export function eventIsInitialMessage(event: SseResponse): event is StreamInitalMessage {
+export function eventIsInitialMessage(event: SseResponse): event is SseResponseInitialMessage {
   return !("entity" in event) || ("entity" in event && event.entity === NotificationEntity.initialMessage);
 }
 
-export function eventIsPoolSwap(event: SseResponse): event is NotificationDtoPoolSwapDto {
+export function eventIsPoolSwap(event: SseResponse): event is SseResponsePoolSwap {
   return "entity" in event && event.entity === NotificationEntity.poolSwap;
 }
 
-export function eventIsStakingPosition(event: SseResponse): event is NotificationDtoStakingPositionDto {
+export function eventIsStakingPosition(event: SseResponse): event is SseResponseStakingPosition {
   return "entity" in event && event.entity === NotificationEntity.stakingPosition;
 }
 
-export function eventIsLendingPosition(event: SseResponse): event is NotificationDtoLendingPositionDto {
+export function eventIsLendingPosition(event: SseResponse): event is SseResponseLendingPosition {
   return "entity" in event && event.entity === NotificationEntity.lendingPosition;
 }
 
-export function eventIsTradeHistory(event: SseResponse): event is NotificationDtoTradeHistoryEntryDto {
+export function eventIsTradeHistory(event: SseResponse): event is SseResponseTradeHistoryEntry {
   return "entity" in event && event.entity === NotificationEntity.tradeHistoryEntry;
 }
 
-export function eventIsOrderHistory(event: SseResponse): event is NotificationDtoOrderHistoryEntryDto {
+export function eventIsOrderHistory(event: SseResponse): event is SseResponseOrderHistoryEntry {
   return "entity" in event && event.entity === NotificationEntity.orderHistoryEntry;
 }
 
-export function eventIsStateSnapshot(event: SseResponse): event is SnapshotContainer {
+export function eventIsStateSnapshot(event: SseResponse): event is SseResponseSnapshot {
   return "entity" in event && event.entity === NotificationEntity.stateSnapshot;
 }
